9ef164bef91a572d2316649bc17bdcb6b878e04e
HardWorkingStation <hws@my.power>
2022-12-06 02:11:56 +0300

    Big update

--- src/app.py
+++ src/app.py
@@ -19,21 +19,34 @@ if step_select == "Описание задачи":
     tools.show_description()
 
 if step_select == "Исследовательский анализ данных":
-    st.write(tools.read_data())
+
+    revenue_05_2022, revenue_06_2022, pass_throw_05, pass_throw_06, sced, airport, airline = tools.read_data()
+    st.write("## 1. Посмотрим на доступные данные")
+    with st.expander("Показать блок с данными"):
+        tools.show_data_samples()
+
+    st.write("## 2. Визуализируем данные")
+    with st.expander("Показать блок с визуализацией"):
+        tools.visualize()
+
 
 if step_select == "Обучение модели прогнозирования":
+
+    user_options = {}
+
     with st.form("learning_config"):
         col1, col2 = st.columns(2)
 
         with col1:
-            add_weather_data = st.checkbox("Добавить данные о погоде")
-            add_mean_revenue = st.checkbox("Добавить среднюю выручку по точке")
-            add_info_from_flight_radar = st.checkbox("Добавить информацию с ресурса flightradar24.com")
-            add_busydays = st.checkbox("Добавить информацию о выходных днях")
+            user_options["add_weather_data"] = st.checkbox("Добавить данные о погоде")
+            user_options["add_mean_revenue"] = st.checkbox("Добавить среднюю выручку по точке")
+            user_options["add_info_from_flight_radar"] = st.checkbox("Добавить информацию с ресурса flightradar24.com")
+            user_options["add_busy_days"] = st.checkbox("Добавить информацию о выходных днях")
 
         with col2:
-            add_day = st.checkbox("Добавить День из даты в отдельную колонку")
-            add_hour = st.checkbox("Добавить Час из даты в отдельную колонку")
-            add_minute = st.checkbox("Добавить Минуту из даты в отдельную колонку")
+            user_options["add_day"] = st.checkbox("Добавить День из даты в отдельную колонку")
+            user_options["add_hour"] = st.checkbox("Добавить Час из даты в отдельную колонку")
+            user_options["add_minute"] = st.checkbox("Добавить Минуту из даты в отдельную колонку")
+            user_options["add_period_of_day"] = st.checkbox("Добавить время суток (утро, день, вечер)")
 
-        st.form_submit_button("Применить")
+        st.form_submit_button("Обучить и оценить модель")
--- src/tools.py
+++ src/tools.py
@@ -1,6 +1,8 @@
 from typing import Union, List, Dict, Tuple
 
+import numpy as np
 import pandas as pd
+import plotly.express as px
 import streamlit as st
 
 
@@ -64,10 +66,176 @@ def show_description():
     )
 
 
-def read_data() -> Union[pd.DataFrame, pd.DataFrame, pd.DataFrame,
-                                  pd.DataFrame, pd.DataFrame, pd.DataFrame,
-                                  pd.DataFrame]:
-    revenue_05_2022 = pd.read_excel("data/05.2022_Выручка.xlsx")
-    revenue_06_2022 = pd.read_excel("data/06.2022_Выручка.xlsx")
+def show_data_samples():
 
-    return revenue_05_2022
+    # т.к. read_data закеширована, то тут просто будет взят кеш, т.к. данные загружаются первый раз в app.py
+    # и не будет повторной загрузки данных с диска
+    revenue_05_2022, revenue_06_2022, pass_throw_05, pass_throw_06, sced, airport, airline = read_data()
+
+    st.write("""
+            ---
+            Данные выручки торговых точек за май:
+            """)
+    st.table(revenue_05_2022.sample(7))
+    st.button("Обновить", key="UPDATE")
+
+    st.write("Статистические данные по выручке за май:")
+    st.dataframe(revenue_05_2022["revenue"].describe())
+
+    st.write("""
+            ---
+            Данные о пассажиропотоке за май:
+            """)
+    st.table(pass_throw_05.sample(7))
+    st.button("Обновить", key="UPDATE 2")
+
+    st.write("""
+            ---
+            Расписание рейсов за май:
+            """)
+    st.table(sced.sample(7))
+    st.button("Обновить", key="UPDATE 3")
+
+    st.write("""
+            ---
+            Справочник AIRLINES:
+            """)
+    st.table(airline.sample(7))
+    st.button("Обновить", key="UPDATE 4")
+
+    st.write("""
+            ---
+            Справочник AIRPORTS:
+            """)
+    st.table(airport.sample(7))
+    st.button("Обновить", key="UPDATE 5")
+
+
+def visualize():
+
+    # т.к. read_data закеширована, то тут просто будет взят кеш, т.к. данные загружаются первый раз в app.py
+    # и не будет повторной загрузки данных с диска
+    revenue_05_2022, revenue_06_2022, pass_throw_05, pass_throw_06, sced, airport, airline = read_data()
+
+    st.write("""
+    ---
+    Данные пассажиропотока по авиакомпаниям:
+    """)
+
+    flights = pd.pivot_table(pass_throw_05,
+                             index=["Дата рейса", "Рейс", "Авиакомпания", "Направление куда летит", "Терминал"],
+                             values=["Вход в чистую зону"],
+                             aggfunc="count").reset_index()
+    flights.rename(columns={'Вход в чистую зону': 'Кол_пассажиров'}, inplace=True)
+
+    av = flights.groupby('Авиакомпания')['Кол_пассажиров'].sum().reset_index().sort_values(by='Кол_пассажиров',
+                                                                                           ascending=False)
+    fig = px.bar(av, x='Авиакомпания', y='Кол_пассажиров', text='Кол_пассажиров')
+    fig.update_layout(
+        xaxis_title_text='Авиакомпания',
+        yaxis_title_text='Количество пассажиров',
+        bargap=0.2,
+        bargroupgap=0.1, font_size=15,
+        width=1400
+    )
+    fig.update_traces(textfont_size=14)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.write("""
+    ---
+    Данные пассажиропотока по терминалу вылета:
+    """)
+
+    fig = px.histogram(
+        x=pass_throw_05['Терминал'].value_counts().index,
+        y=pass_throw_05['Терминал'].value_counts(),
+        text_auto=True
+    )
+
+    fig.update_traces(textfont_size=14)
+
+    fig.update_layout(
+        xaxis_title_text='Терминал',
+        yaxis_title_text='Количество пассажиров',
+        bargap=0.2,
+        bargroupgap=0.1, font_size=15,
+        width=1400
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+
+@st.cache(show_spinner=False)
+def read_data() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame,
+                         pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+
+    with st.spinner("Загрузка данных..."):
+        revenue_05_2022 = pd.read_excel("data/05.2022_Выручка.xlsx")
+        revenue_05_2022 = prepare_revenue(revenue_05_2022)
+
+        revenue_06_2022 = pd.read_excel("data/06.2022_Выручка.xlsx")
+        revenue_06_2022 = prepare_revenue(revenue_06_2022)
+
+        pass_throw_05 = pd.read_excel("data/05.2022_Пассажиропоток.xlsx")
+        pass_throw_06 = pd.read_excel("data/06.2022_Пассажиропоток.xlsx")
+
+        sced = pd.read_excel("data/Расписание рейсов 05-06.2022.xlsx")
+
+        airport = pd.read_excel("data/Справочник_AIRPORTS.xlsx")
+        airline = pd.read_excel("data/Справочник_AIRLINES.xlsx")
+
+    return revenue_05_2022, revenue_06_2022, pass_throw_05, pass_throw_06, sced, airport, airline
+
+
+def prepare_revenue(revenue: pd.DataFrame) -> pd.DataFrame:
+    revenue = revenue.rename(columns={"Дата": "date",
+                                      "Дата с разбивкой по 30 минут": "timeThirty",
+                                      "Прибыль на момент времени": "revenue",
+                                      "Точка продаж": "point",
+                                      })
+
+    revenue['revenue'] = revenue['revenue'].apply(lambda x: str(x).replace(u'\xa0', u''))
+    revenue['revenue'] = revenue['revenue'].apply(lambda x: str(x).replace(u',', u'.'))
+    revenue["revenue"] = revenue["revenue"].astype(float)
+
+    revenue["timeThirty"] = pd.to_datetime(revenue["timeThirty"], utc=True)
+    revenue["day_of_week"] = revenue["timeThirty"].dt.dayofweek
+    revenue["day_of_year"] = revenue["timeThirty"].dt.dayofyear
+    revenue["day"] = revenue["timeThirty"].dt.day
+    revenue["month"] = revenue["timeThirty"].dt.month
+    revenue["hour"] = revenue["timeThirty"].dt.hour
+    revenue["minutes"] = revenue["timeThirty"].dt.minute
+
+    return revenue
+
+
+def add_weekend_revenue_06(revenue: pd.DataFrame) -> pd.DataFrame:
+
+    revenue["is_weekend"] = np.logical_or(False, revenue["day_of_week"] == 6)
+    revenue["is_weekend"] = np.logical_or(revenue["is_weekend"], revenue["day_of_week"] == 7)
+    revenue["is_weekend"] = np.logical_or(revenue["is_weekend"], revenue["day"] == 13)
+    revenue["is_weekend"] = revenue["is_weekend"].astype(int)
+
+    return revenue
+
+
+def data_prepare_by_user_choice(user_options: dict, data: pd.DataFrame):
+    if user_options["Добавить среднюю выручку по точке"]:
+        mean_revenue = (data[["point", "revenue"]]
+                        .groupby(["point"])
+                        .mean()
+                        .sort_values(by="revenue")
+                        .reset_index())\
+            .rename(columns={"revenue": "mean_revenue"})
+        data = data.merge(mean_revenue, how='left', left_on='point', right_on='point')
+        del mean_revenue
+
+    if user_options[""]:
+        data["is_weekend"] = np.logical_or(False, data["day_of_week"] == 6)
+        data["is_weekend"] = np.logical_or(data["is_weekend"], data["day_of_week"] == 7)
+        data["is_weekend"] = np.logical_or(data["is_weekend"], data["day"] == 2)
+        data["is_weekend"] = np.logical_or(data["is_weekend"], data["day"] == 3)
+        data["is_weekend"] = np.logical_or(data["is_weekend"], data["day"] == 9)
+        data["is_weekend"] = np.logical_or(data["is_weekend"], data["day"] == 10)
+        data["is_weekend"] = data["is_weekend"].astype(int)
